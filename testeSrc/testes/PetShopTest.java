3917490ab7a2599dffc3754d09a99f7bf99623e5
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2016-08-02 21:52:01 -0300

    Commit de teste

---
+++ testeSrc/testes/PetShopTest.java
@@ -0,0 +1,14 @@
+package testes;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PetShopTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
